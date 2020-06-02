7f81ab02c7947d64c78be557f7f339170c6b5fa0
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-02 14:14:10 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,45 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+type Fecha = (Int, Int, Int)
+type CondicionViaje = Viaje -> Bool
+
+-- 1)
+
+data Chofer = UnChofer {
+    nombreChofer :: String,
+    kilometraje :: Int,
+    viajes :: [Viaje],
+    condicionViaje :: CondicionViaje
+}deriving (Show)
+
+data Cliente = UnCliente {
+    nombreCliente :: String,
+    vivienda :: String
+}deriving (Show)
+
+data Viaje = UnViaje {
+    fecha :: Fecha,
+    cliente :: Cliente,
+    costo :: Float
+}deriving (Show)
+
+
+-- 2)
+
+-- Ejemplos: 
+
+clientePrueba = UnCliente {
+    nombreCliente = "Paulita",
+    vivienda = "Devoto"
+}
+
+viajePrueba = UnViaje {
+    fecha = (02,06,2020),
+    cliente = (UnCliente "Paulita" "Devoto"),
+    costo = 1200.05
+}
+
+
+
